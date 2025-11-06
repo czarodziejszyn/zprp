839b2b1aa6b2c089dde27cd02a7a2e8767950494
Szymon <01178585@pw.edu.pl>
2025-11-06 14:51:02 +0100

    feat: one record class and dataset class

--- zprp/data-science/data_record.py
+++ zprp/data-science/data_record.py
@@ -1,2 +1,4 @@
 class DataRecord:
-    def __init__(self, features, label):
+    def __init__(self, circle):
+        self.features = get_features(circle)
+        self.label = get_label()
--- zprp/data-science/dataset.py
+++ zprp/data-science/dataset.py
@@ -1,4 +1,20 @@
+import pandas as pd
+from data_record import DataRecord
+
+
 class Dataset:
     def __init__(self, circle: int, size: int):
         self.circle = circle
         self.size = size
+        self.features = []
+        self.label = None
+
+    def create(self):
+        records = [DataRecord(self.circle) for _ in range(self.size)]
+        data = [r.features | r.label for r in records]
+        return pd.DataFrame(data)
+
+    def save_to_csv(self, path: str):
+        df = self.create()
+        df.to_csv(path)
+        return df
