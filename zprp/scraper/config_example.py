2c767a167483e9aa281a435b4c45ea937f37baad
Julia Czyzewska <julaczyzewska28@gmail.com>
2025-12-07 22:48:55 +0100

    refactor: moved /scraper from /backend dir to its own separate dir
                Issue: #17

---
+++ zprp/scraper/config_example.py
@@ -0,0 +1,9 @@
+NOMINATIM_URL = "https://nominatim.openstreetmap.org/search"
+OTODOM_URL = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/warszawa"
+
+OFFERS_JSON_PATH = r"otodom_warszawa.json"
+
+# to customize 
+CHROMEDRIVER_PATH = r"C:\chromedriver\chromedriver.exe"
+CHROME_BINARY_PATH = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+MAX_OFFER_PAGES = 1
