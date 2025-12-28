fd3d7d285a5b7ee366296e01f42855aa135e2dbe
Julia Czyzewska <julaczyzewska28@gmail.com>
2025-12-29 00:44:30 +0100

    refactor: updated swagger docs after refactoring of fetch fucnitons

--- zprp/backend/api/main.py
+++ zprp/backend/api/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI, Query
 from fastapi.responses import FileResponse
 from pathlib import Path
 
-from api.police_stations import fetch_police_stations, PoliceStation
-from api.aeds import fetch_aeds, AEDs
-from api.pharmacies import fetch_pharmacies, Pharmacy
-from api.nature import fetch_nature, Nature
-from api.bike_stations import fetch_bike_stations, BikeStation
-from api.accomodations import fetch_accommodations, Accommodation
-from api.stops import fetch_stops, Stop
-from api.theatres import fetch_theatres, Theatre
-from api.attractions import fetch_attractions, Attraction
+from api.police_stations import fetch_police_stations
+from api.aeds import fetch_aeds
+from api.pharmacies import fetch_pharmacies
+from api.nature import fetch_nature
+from api.bike_stations import fetch_bike_stations
+from api.accomodations import fetch_accommodations
+from api.stops import fetch_stops
+from api.theatres import fetch_theatres
+from api.fetch_warsaw_api import WarsawApiObj
+
+
+from api.attractions import fetch_attractions
 from db.count_objects_nearby import count_objects_nearby
 from db.get_avg_real_price import get_avg_real_price
 from model_use.run_model import calculate_prices, create_chart
@@ -19,7 +22,6 @@ from model_use.run_model import calculate_prices, create_chart
 app = FastAPI(title="Warsaw Public Safety API")
 
 # Endpoints for model
-
 @app.get("/nearby")
 def nearby(lat: float, lon: float, radius: int):
     result = count_objects_nearby(lat, lon, radius)
@@ -52,17 +54,23 @@ def get_chart():
 
 
 # Endpoints for database
-@app.get("/police_stations", response_model=list[PoliceStation])
+@app.get("/police_stations", response_model=list[WarsawApiObj])
 async def get_police_stations():
+    """
+    Get list of police stations in Warsaw.
+    """
     return await fetch_police_stations()
 
 
-@app.get("/aeds", response_model=list[AEDs])
+@app.get("/aeds", response_model=list[WarsawApiObj])
 async def get_aeds():
+    """
+    Get list of AEDS in Warsaw.
+    """
     return await fetch_aeds()
 
 
-@app.get("/pharmacies", response_model=list[Pharmacy])
+@app.get("/pharmacies", response_model=list[WarsawApiObj])
 async def get_pharmacies():
     """
     Get list of pharmacies in Warsaw.
@@ -70,31 +78,31 @@ async def get_pharmacies():
     return await fetch_pharmacies()
 
 
-@app.get("/nature/{dataset_name}", response_model=list[Nature])
-async def get_nature(dataset_name: str):
+@app.get("/nature", response_model=list[WarsawApiObj])
+async def get_nature():
     """
-    Get nature data by dataset_name (bushes, forests, trees).
+    Get list of nature objects (bushes, forests, trees).
     """
-    return await fetch_nature(dataset_name)
+    return await fetch_nature()
 
 
-@app.get("/bike_stations", response_model=list[BikeStation])
+@app.get("/bike_stations", response_model=list[WarsawApiObj])
 async def get_bike_stations():
     """
     Get a list of Warsaw bicycle stations.
     """
-    return await fetch_bike_stations("bike_stations")
+    return await fetch_bike_stations()
 
 
-@app.get("/accommodations/{dataset_name}", response_model=list[Accommodation])
-async def get_accommodations(dataset_name: str):
+@app.get("/accommodations", response_model=list[WarsawApiObj])
+async def get_accommodations():
     """
     Get list of accommodations (hotels or university dorms).
     """
-    return await fetch_accommodations(dataset_name)
+    return await fetch_accommodations()
 
 
-@app.get("/stops", response_model=list[Stop])
+@app.get("/stops", response_model=list[WarsawApiObj])
 async def get_stops():
     """
     Get a list of Warsaw public transport stops.
@@ -102,11 +110,17 @@ async def get_stops():
     return await fetch_stops()
 
 
-@app.get("/theatres", response_model=list[Theatre])
+@app.get("/theatres", response_model=list[WarsawApiObj])
 async def get_theatres():
+    """
+    Get list of theatres in Warsaw.
+    """
     return await fetch_theatres()
 
 
-@app.get("/tourist_attractions", response_model=list[Attraction])
+@app.get("/tourist_attractions", response_model=list[WarsawApiObj])
 async def get_attractions():
+    """
+    Get list of tourist attractions in Warsaw.
+    """
     return await fetch_attractions()
