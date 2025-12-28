d9fdf66c88c4925ebbea3ec62724155926f128c3
Julia Czyzewska <julaczyzewska28@gmail.com>
2025-12-28 17:32:33 +0100

    Implemented functions that allow wirting Warsaw API data into .json file as a form of cache. Issue: #23

---
+++ zprp/backend/cache/json_cache.py
@@ -0,0 +1,13 @@
+import json
+import os
+API_CACHE_JSON_PATH = os.getenv("API_CACHE_JSON_PATH")
+
+def save_json(data):
+    with open(API_CACHE_JSON_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def load_json():
+    if not os.path.exists(API_CACHE_JSON_PATH):
+        raise FileNotFoundError(f"Backup JSON not found: {API_CACHE_JSON_PATH}")
+    with open(API_CACHE_JSON_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
