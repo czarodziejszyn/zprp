b27cef8dcc174ca80fa973b6b91a4739229c37a5
Szymon <01178585@pw.edu.pl>
2026-01-09 14:01:58 +0100

    feat: best model selection with saving

--- zprp/data_science/create_dataset.py
+++ zprp/data_science/create_dataset.py
@@ -1,4 +1,4 @@
 from dataset import Dataset
 
-creator = Dataset(500, "../data/processed/geocoded_otodom_warszawa.json")
-creator.save_to_csv("warsaw_house_data.csv")
+creator = Dataset(700, "../data/processed/geocoded_otodom_warszawa.json")
+creator.save_to_csv("warsaw_house_data_700.csv")
